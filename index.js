90070cf39a0299c7eb533d537067d83c9eb7fe40
Beau Lazear <beaulazear@gmail.com>
2022-10-23 20:20:22 -0400

    commit tree dce7cf59b3ae6efa924a6762d008aeabb4b70c91

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: `POST`,
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    }
+    return fetch(`http://localhost:3000/users`,configurationObject)
+    .then(resp => resp.json())
+    .then(data => document.body.append(data.id))
+    .catch(error => document.body.append(error.message))
+}
